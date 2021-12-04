011f12fbf41d69fe8ccdc8472d879c52e4660575
Dhanashree Yadav <86838041+DhanuY@users.noreply.github.com>
2021-12-04 17:33:56 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+document.querySelector('#push').onclick = function () {
+    if (document.querySelector('#newtask input').value.length == 0) {
+        alert("Please enter a task.")
+    }
+    else {
+        document.querySelector('#tasks').innerHTML += `
+        <div class = "tasks">
+             <span id = "taskname">
+               ${document.querySelector('#newtask input').value}
+             </span>
+
+             <button class = "delete"> Delete </buton>
+        </div>
+        `;
+
+        var current_task = document.querySelectorAll(".delete");
+        for (var i = 0; i < current_task.length; i++) {
+            current_task[i].onclick = function () {
+                this.parentNode.remove();
+            }
+
+        }
+        document.querySelector("#newtask input").value = "";
+    }
+
+
+
+}
